bb9a8d23f4009732ff326a9e0f7a6f8a0a589249
hangyeong-eun <az2282@naver.com>
2022-11-12 23:01:35 +0900

    mediInstitution 수정 전

--- Mid-termProject/mediInstitution.py
+++ Mid-termProject/mediInstitution.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ServiceKey = "rX0WAdrv4MYSvgvMgVKuBiNN1%2BkHA2lDHgbzzvOI%2F95pXZrq%2FlmMj0jt6KCI1NMHGfV0BQTPOsd7kPoAeT7p9Q%3D%3D"
 
@@ -46,14 +47,13 @@ def getItem(apiType):
 
 # [CODE 3]
 def getService(apiType):
-    # jsonResult = []
+    jsonResult = []
     result = []
     year = ''
     dvsd = ''
     ttl = ''
     jsonData = getItem(apiType)
 
-    print(json.dumps(jsonData, indent=4, sort_keys=True, ensure_ascii=False))
     if (jsonData['resultCode'] == '00'):
         if jsonData['items'] == '':
             print("데이터 없음....")
@@ -67,26 +67,59 @@ def getService(apiType):
                 ttl = jsonData['items'][i]['ttl']
                 print('[ %s %s %s ]' % (year, dvsd, ttl))
                 print('----------------------------------------------------------------------')
-                # jsonResult.append({'year': year, 'dvsd': dvsd,'totalCount': totalCount, 'sum': sum})
+                jsonResult.append({'year': year, 'dvsd': dvsd,'totalCount': totalCount, 'sum': sum})
                 result.append([year, dvsd, ttl])
 
-            return (result, year, dvsd, ttl)
+            return (jsonResult, result, year, dvsd, ttl)
 
 
 def main():
-    # jsonResult = []
+    jsonResult = []
     result = []
     print(">>>>>>>> 크롤링 시작")
     apiType = "JSON"
 
 
-    result, year, dvsd, ttl = getService(apiType)
+    jsonResult, result, year, dvsd, ttl = getService(apiType)
 
 
+    #파일 저장
     columns = ["년도", "지역", "병원 수"]
     result = pd.DataFrame(result, columns=columns)
     result.to_csv('hospitals.csv', index=False, encoding='utf-8')
 
+
+
+    ##그래프 그리기
+    import matplotlib
+    from matplotlib import font_manager, rc
+
+    # 1) 그래프의 Y축 데이터 : 방문객 수 visitCnt[]
+    # 2) 그래프의 X축 데이터 : 방문 년월 visitYM[]
+    visitCnt = []
+    visitYM = []
+    index = []
+    i = 0
+    for item in jsonResult:
+        index.append(i)
+        visitCnt.append(item['dvsd'])
+        visitYM.append(item['year'])
+        i += 1
+
+    # font_path = "/Users/hangyeong-eun/Documents/My_Python/malgun.ttf"
+    # font = font_manager.FontProperties(fname=font_path).get_name()
+    # matplotlib.rc('font', family=font)
+
+    rc('font', family='AppleGothic')
+    plt.rcParams['axes.unicode_minus'] = False
+
+
+
+    plt.xticks(index, visitYM)
+    plt.plot(index, visitCnt)
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == '__main__':
--- Mid-termProject/mediWorker.py
+++ Mid-termProject/mediWorker.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import json
 import pandas as pd
+from matplotlib import font_manager, rc
 
 ServiceKey = "rX0WAdrv4MYSvgvMgVKuBiNN1%2BkHA2lDHgbzzvOI%2F95pXZrq%2FlmMj0jt6KCI1NMHGfV0BQTPOsd7kPoAeT7p9Q%3D%3D"
 
@@ -24,20 +25,18 @@ def getRequestUrl(url):
 
 
 # [CODE 2]
-def getItem(apiType,dvsd):
+def getItem(apiType, dvsd, year):
     service_url = "http://apis.data.go.kr/1352000/ODMS_STAT_39/callStat39Api"
     parameters = "?_type=json&serviceKey=" + ServiceKey  # 인증키
     parameters += "&pageNo=" + "1"
     parameters += "&numOfRows=" + "85"
     parameters += "&apiType=" + apiType
-    # parameters += "&year=" + "2015"
+    parameters += "&year=" + str(year)
     parameters += "&dvsd=" + dvsd
 
     url = service_url + parameters
 
     retData = getRequestUrl(url)  # [CODE 1]
-
-    # print(retData)
 
     if (retData == None):
         return None
@@ -46,49 +45,91 @@ def getItem(apiType,dvsd):
 
 
 # [CODE 3]
-def getService(apiType, dvsd):
-    # jsonResult = []
-    result = []
-    year = ''
-    dvsd = ''
+def getService(apiType, dvsd, year, result, mpl):
     sum = ''
-    # totalCount = ''
-    jsonData = getItem(apiType, dvsd)
+    dvsd = ''
+    jsonData = getItem(apiType, dvsd, year)
 
     if (jsonData['resultCode'] == '00'):
         if jsonData['items'] == '':
             print("데이터 없음....")
-
         else:
-            print(json.dumps(jsonData, indent=4,
-                                 sort_keys=True, ensure_ascii=False))
-            for i in range(0,85):
+            print(json.dumps(jsonData, indent=4, sort_keys=True, ensure_ascii=False))
+            tmp = []
+            for i in range(len(jsonData['items'])):
                 year = jsonData['items'][i]['year']
                 dvsd = jsonData['items'][i]['dvsd']
-                # totalCount = jsonData['totalCount']
                 sum = jsonData['items'][i]['sum']
                 print('[ %s %s %s ]' % (year, dvsd, sum))
                 print('----------------------------------------------------------------------')
-                # jsonResult.append({'year': year, 'dvsd': dvsd,'totalCount': totalCount, 'sum': sum})
+                tmp.append([year, dvsd, sum])
                 result.append([year, dvsd, sum])
-
-        return (result, year, dvsd, sum)
+            mpl.append(tmp)
+        return (result, year, dvsd, sum, mpl)
 
 
 def main():
-    # jsonResult = []
     result = []
+    mpl = []
     print(">>>>>>>> 크롤링 시작")
     apiType = "JSON"
-    dvsd = "경기"
-
-
-    result, sum, dvsd, year = getService(apiType, dvsd)
+    dvsd = ""
+    yr = [2015, 2016, 2017, 2018, 2019]
+    for i in yr:
+        result, sum, dvsd, year, mpl = getService(apiType, dvsd, i, result, mpl)
+    print(result)
+    print(mpl)
 
     columns = ["년도", "지역", "의료 종사자 수"]
     result = pd.DataFrame(result, columns=columns)
     result.to_csv('sample.csv', index=False, encoding='utf-8')
 
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    def graph1(mpl):
+        for num, i in enumerate(mpl):
+            lst, tmp = [], []
+            x = np.arange(17)
+            for j in i:
+                tmp = [j[1], int(j[2])]
+                lst.append(tmp)
+                lst.sort(key=lambda x: x[0])
+            rg, nm = [], []
+            for i in lst:
+                rg.append(i[0])
+                nm.append(i[1])
+
+            plt.bar(x, nm)
+            plt.title(str(num + 2015) + "년도 보건의료기관 소재지별 의료인력수")
+            plt.xticks(x, rg)
+            plt.show()
+
+    graph1(mpl)
+
+    def graph2(mpl):
+        nm = []
+        r = [[] for _ in range(17)]
+        years = [2015, 2016, 2017, 2018, 2019]
+        name = []
+
+        for bf, i in enumerate(mpl):
+            i.sort(key=lambda x: x[1])
+            for num, j in enumerate(i):
+                r[num].append(int(j[2]))
+                if bf == 0:
+                    name.append(j[1])
+
+        for i in range(17):
+            plt.plot(years, r[i], "-o", label=name[i])
+            plt.xticks(years)
+            plt.legend()
+
+        plt.xlabel('년도')
+        plt.ylabel('의료 인력 수')
+        plt.show()
+
+    graph2(mpl)
 
 
 if __name__ == '__main__':
